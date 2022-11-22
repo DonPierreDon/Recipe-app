dad44de0220320cccf235879039caa9fa23f7839
KrackBoulba <DonPierre@gmx.com>
2022-11-22 12:02:18 +0100

    S16 end codeCheck

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,9 @@
-# Database models.
+"""
+Database models.
+"""
+
+import uuid
+import os
 
 from django.conf import settings
 from django.db import models
@@ -8,6 +13,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
     # Manager for users.
@@ -57,7 +70,8 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
-    ingredients = models.ManyToManyField('ingredient')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -62,7 +62,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         auth_user = self.context['request'].user
         for ingredient in ingredients:
-            ingredient_obj, create = Ingredient.objects.get_or_create(
+            ingredient_obj, created = Ingredient.objects.get_or_create(
                 user=auth_user,
                 **ingredient,
             )
@@ -83,9 +83,13 @@ class RecipeSerializer(serializers.ModelSerializer):
         """Update Recipe"""
 
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
@@ -101,3 +105,11 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + ['description']
 
 
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
